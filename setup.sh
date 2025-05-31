17bd8d7546e5c129cac552a36ab08d03ed02788a
eze1087 <82317761+eze1087@users.noreply.github.com>
2025-05-31 01:44:45 -0300

    setup.sh

--- setup.sh
+++ setup.sh
@@ -6,14 +6,28 @@ REPO_URL="https://github.com/tuusuario/tu-repo.git"
 # Clonar tu repositorio
 git clone $REPO_URL /tmp/mi-proxy
 
-# Mover el script a la ubicación deseada
-sudo mv /tmp/mi-proxy/proxy.py /etc/SSHPlus/
+# Mover el script PDirect.py a la ubicación deseada
+sudo mv /tmp/mi-proxy/PDirect.py /etc/SSHPlus/
 
 # Dar permisos de ejecución
-sudo chmod +x /etc/SSHPlus/proxy.py
+sudo chmod +x /etc/SSHPlus/PDirect.py
 
-# Crear el archivo del servicio systemd
-echo -e "[Unit]\nDescription=Ejecutar proxy en una sesión de screen\nAfter=network.target\n\n[Service]\nExecStart=/usr/bin/screen -DmS PDirect /usr/bin/python3 /etc/SSHPlus/proxy.py\nWorkingDirectory=/etc/SSHPlus\nUser=root\nRestart=on-failure\nRestartSec=5\n\n[Install]\nWantedBy=multi-user.target" | sudo tee /etc/systemd/system/pdirect.service
+# Crear el archivo del servicio systemd con tu contenido
+sudo tee /etc/systemd/system/pdirect.service > /dev/null << EOL
+[Unit]
+Description=Ejecutar PDirect en una sesión de screen
+After=network.target
+
+[Service]
+ExecStart=/usr/bin/screen -DmS PDirect /usr/bin/python3 /etc/SSHPlus/PDirect.py
+WorkingDirectory=/etc/SSHPlus
+User=root
+Restart=on-failure
+RestartSec=5
+
+[Install]
+WantedBy=multi-user.target
+EOL
 
 # Recargar systemd y arrancar el servicio
 sudo systemctl daemon-reload
